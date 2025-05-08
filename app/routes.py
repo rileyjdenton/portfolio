6a03a63c90d58ca3fb823b642f9aeb4b698d80b7
rileydenton <rileyd315@gmail.com>
2025-05-08 17:26:45 -0400

    Building base.html and base.css

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,23 @@ from . import app
 @app.route('/')
 def index():
         return render_template('index.html')
+
+@app.route('/bio')
+def bio():
+        return render_template('bio.html')
+
+@app.route('/projects')
+def projects():
+        return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+        return render_template('contact.html')
+
+@app.route('/resume')
+def resume():
+        return render_template('resume.html')
+
+@app.route('/admin')
+def admin():
+        return render_template('admin.html')
